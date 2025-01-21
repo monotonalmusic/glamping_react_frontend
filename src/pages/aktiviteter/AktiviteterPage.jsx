e63205874e482fad34322a6bdab771b52ad839d5
Gabriel <gehughes28@gmail.com>
2025-01-21 18:10:39 +0100

    Small changes

--- src/pages/aktiviteter/AktiviteterPage.jsx
+++ src/pages/aktiviteter/AktiviteterPage.jsx
@@ -21,9 +21,10 @@ const AktiviteterPage = () => {
 
 
   return (
-    <div>
+    <div className={page.pageContainer}>
       <PageHeader title={"Aktiviteter"} bgImg={"/image_04.jpg"}></PageHeader>
-      <Box title={"Ingen skal kede sig hos Gitte"}>
+      <div className={styles.aktiviteterPage}>
+      <Box title={"Ingen skal kede sig hos Gitte"} width={"800px"} height={"auto"} color={"#33626C"}>
         Glamping er mere end blot en indkvartering - det er en mulighed for at
         fordybe dig i naturen og skabe minder, der varer livet ud. Uanset om du
         foretrækker en eventyrlig kanotur, en oplysende naturvandring,
@@ -31,9 +32,8 @@ const AktiviteterPage = () => {
         eller morgenyoga, der giver dig indre ro og balance i naturens skød -
         vil vi hos Gittes Glamping imødekomme dine ønsker
       </Box>
+      <ActivityListFront activeActivities={activities}></ActivityListFront>
 
-      <div>
-        <ActivityListFront activeActivities={activities}></ActivityListFront>
 
       </div>
     </div>
